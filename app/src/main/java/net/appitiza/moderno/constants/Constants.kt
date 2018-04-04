6d700f8031a873b834ca1df747546c0c1b10ab60
appitiza <appitiza@gmail.com>
2018-04-04 17:34:57 +0530

    start up added

---
+++ app/src/main/java/net/appitiza/moderno/constants/Constants.kt
@@ -0,0 +1,8 @@
+package net.appitiza.moderno.constants
+
+class Constants {
+    companion object {
+        const val PREF_KEY_IS_USER_LOGGED_IN = "isUserLoggedIn"
+        const val PREF_NAME = "prefModerno"
+    }
+}
