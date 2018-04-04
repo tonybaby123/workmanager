82ff2843167904a277fca727fb20e49d7d4d1f3d
appitiza <appitiza@gmail.com>
2018-04-04 22:35:09 +0530

    ui changes

---
+++ app/src/main/java/net/appitiza/moderno/ui/model/temp.java
@@ -0,0 +1,13 @@
+package net.appitiza.moderno.ui.model;
+
+public class temp {
+    private  String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
