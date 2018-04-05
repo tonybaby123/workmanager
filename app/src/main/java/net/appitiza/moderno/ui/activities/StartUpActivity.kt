e2eee2d0a244346094e31ee93e362233ff36a2d5
appitiza <appitiza@gmail.com>
2018-04-05 13:27:30 +0530

    color added

--- app/src/main/java/net/appitiza/moderno/ui/activities/StartUpActivity.kt
+++ app/src/main/java/net/appitiza/moderno/ui/activities/StartUpActivity.kt
@@ -1,7 +1,9 @@
 package net.appitiza.moderno.ui.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_start_up.*
 import net.appitiza.moderno.R
 
 class StartUpActivity : AppCompatActivity() {
@@ -9,5 +11,6 @@ class StartUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_up)
+        tv_login_login.setOnClickListener { startActivity(Intent(this@StartUpActivity,AdminActivity::class.java)) }
     }
 }
