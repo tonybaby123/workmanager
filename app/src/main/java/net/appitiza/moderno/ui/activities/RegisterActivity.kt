30849d80982df0f11e6f90c3773cc45f7953c64e
appitiza <appitiza@gmail.com>
2018-04-05 20:12:26 +0530

    style added

--- app/src/main/java/net/appitiza/moderno/ui/activities/RegisterActivity.kt
+++ app/src/main/java/net/appitiza/moderno/ui/activities/RegisterActivity.kt
@@ -1,13 +1,87 @@
 package net.appitiza.moderno.ui.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_register.*
 import net.appitiza.moderno.R
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
+   // private var mProgress: ProgressDialog? = null
+
+    //Firebase auth
+    private var mAuth: FirebaseAuth? = null
+    //Firebase referrence
+    private var mDatabase: DatabaseReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        initializeFireBase()
+        tv_register_register.setOnClickListener { registerUser(et_register_name.text.toString(), et_register_email.text.toString(), et_register_password.text.toString()) }
+    }
+
+    private fun initializeFireBase() {
+      //  mProgress = ProgressDialog(this)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerUser(displayname: String, email: String, password: String) {
+        if (validation(displayname, email, password)) {
+           /* mProgress?.setTitle("Registering")
+            mProgress?.setMessage("Please wait..")
+            mProgress?.setCancelable(false)
+            mProgress?.show()
+*/
+            mAuth?.createUserWithEmailAndPassword(email, password)
+                    ?.addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+
+                            // Sign in success, update UI with the signed-in user's information
+                            val user = mAuth?.getCurrentUser()
+                            val uid = user!!.uid
+                            mDatabase = FirebaseDatabase.getInstance().reference.child("User").child(uid)
+                            val map = HashMap<String, String>()
+                            map["name"] = displayname
+                            map["type"] = "user"
+                            mDatabase?.setValue(map)?.addOnCompleteListener { reg_task ->
+                                if (reg_task.isSuccessful) {
+                                  //  mProgress!!.dismiss()
+                                    finish()
+
+                                } else {
+                                //    mProgress!!.hide()
+                                    Toast.makeText(this@RegisterActivity, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show()
+                                }
+                            }
+
+                        } else {
+                          //  mProgress!!.hide()
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this@RegisterActivity, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show()
+                        }
+                    }
+        } else {
+            Toast.makeText(this@RegisterActivity, "incomplete",
+                    Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validation(displayname: String, email: String, password: String): Boolean {
+        if (displayname.equals("")) {
+            return false
+        } else if (email.equals("")) {
+            return false
+        } else if (password.equals("")) {
+            return false
+        } else {
+            return true
+        }
     }
 }
--- app/src/main/java/net/appitiza/moderno/ui/activities/StartUpActivity.kt
+++ app/src/main/java/net/appitiza/moderno/ui/activities/StartUpActivity.kt
@@ -6,18 +6,28 @@ import android.support.v4.app.ActivityOptionsCompat
 import android.support.v4.util.Pair
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import kotlinx.android.synthetic.main.activity_register.*
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_start_up.*
 import net.appitiza.moderno.R
 import net.appitiza.moderno.ui.activities.admin.AdminActivity
 
 class StartUpActivity : AppCompatActivity() {
 
+   // private var mProgress: ProgressDialog? = null
+    //Firebase auth
+    private var mAuth: FirebaseAuth? = null
+    private var mDatabase: DatabaseReference? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_up)
+        initialize()
         tv_login_login.setOnClickListener {
-            startActivity(Intent(this@StartUpActivity, AdminActivity::class.java))
+
+            loginUser(et_login_email.text.toString(), et_login_password.text.toString())
         }
         tv_login_register.setOnClickListener {
 
@@ -29,8 +39,61 @@ class StartUpActivity : AppCompatActivity() {
             val p3 = Pair(tv_password_txt as View, getString(R.string.passwordtext_login_register))
             val p4 = Pair(et_login_password as View, getString(R.string.password_login_register))
             val p5 = Pair(tv_login_register as View, getString(R.string.register_login_register))
-            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this@StartUpActivity, p1, p2, p3, p4,p5)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this@StartUpActivity, p1, p2, p3, p4, p5)
             startActivity(intent, options.toBundle())
+        }
+    }
+
+    private fun initialize() {
+     //   mProgress = ProgressDialog(this)
+        //Firebase auth
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance().reference
+    }
+
+    private fun loginUser(email: String, password: String) {
+        if (validation(email, password)) {
+        /*    mProgress?.setTitle("Registering")
+            mProgress?.setMessage("Please wait..")
+            mProgress?.setCancelable(false)
+            mProgress?.show()*/
+
+            mAuth?.signInWithEmailAndPassword(email, password)?.addOnCompleteListener(this)
+            { task ->
+                if (task.isSuccessful) {
+                   // mProgress?.dismiss()
+
+                    val deviceToken = FirebaseInstanceId.getInstance().token
+                    val userId = mAuth?.getCurrentUser()!!.uid
+                    mDatabase?.child("User")?.child(userId)?.child("device_token")?.setValue(deviceToken)?.addOnSuccessListener {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = mAuth?.getCurrentUser()
+                        val intent = Intent(this@StartUpActivity, AdminActivity::class.java)
+                        startActivity(intent);
+                    }
+
+                } else {
+                  //  mProgress?.hide()
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@StartUpActivity, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+        } else {
+            Toast.makeText(this@StartUpActivity, "incomplete",
+                    Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validation(email: String, password: String): Boolean {
+        if (email.equals("")) {
+            return false
+        } else if (password.equals("")) {
+            return false
+        } else {
+            return true
         }
     }
 }
