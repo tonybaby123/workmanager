4a0c1133a83e33262bfd842b583dcc92553e4691
tonybaby123 <tonybaby1990@gmail.com>
2018-04-05 16:37:36 +0530

    user and admmin activities

---
+++ app/src/main/java/net/appitiza/moderno/ui/activities/admin/AdminWorkReportsActivity.kt
@@ -0,0 +1,13 @@
+package net.appitiza.moderno.ui.activities.admin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import net.appitiza.moderno.R
+
+class AdminWorkReportsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_work_reports)
+    }
+}
---
+++ app/src/main/java/net/appitiza/moderno/ui/activities/users/UserHistoryActivity.kt
@@ -0,0 +1,13 @@
+package net.appitiza.moderno.ui.activities.users
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import net.appitiza.moderno.R
+
+class UserHistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_history)
+    }
+}
